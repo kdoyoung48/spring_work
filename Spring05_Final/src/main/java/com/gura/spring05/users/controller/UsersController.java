852107187b7559aa985ba101e28a2c38dfdc4141
kdoyoung <kdoyoung48@gmail.com>
2021-01-27 11:18:19 +0900

    Spring05 /users/loginform.do 요청 처리함

---
+++ Spring05_Final/src/main/java/com/gura/spring05/users/controller/UsersController.java
@@ -0,0 +1,66 @@
+package com.gura.spring05.users.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.gura.spring05.users.dto.UsersDto;
+import com.gura.spring05.users.service.UsersService;
+
+@Controller
+public class UsersController {
+	
+	@Autowired
+	private UsersService service;
+	
+	//로그인 폼 요청처리
+	@RequestMapping("/users/loginform")
+	public ModelAndView loginform(HttpServletRequest request,
+			ModelAndView mView) {
+		//로그인 폼에 관련된 로직을 서지스를 통해서 처리한다.
+		service.loginformLogic(request, mView);
+		//view page정보도 담는다.
+		mView.setViewName("users/loginform");
+		//리턴
+		return mView;
+	}
+	
+	//회원 가입 요청처리
+	//form 전송은 보통 post 방식 요청인데 post 방식 요청만 받아들이도록
+	//컨트롤러에 설정하는게 일반적이다.
+	@RequestMapping(value = "/users/signup", method = RequestMethod.POST)
+	public String signup(@ModelAttribute("dto") UsersDto dto) {
+		/*
+		 * Dto 인 경우에 @ModelAttribute("key 값") 으로 설정하면
+		 * 해당 Dto 가 request 영역에 설정한 "key 값" 으로 담긴다
+		 */
+		service.addUser(dto);
+		return "users/signup";
+	}
+	@RequestMapping("/users/signup_form")
+	public String signupForm() {
+		return "users/signup_form";
+	}
+	
+	//ajax 요청처리
+	@RequestMapping("/users/checkid")
+	public ModelAndView checkid(@RequestParam String inputId, 
+			ModelAndView mView) {
+		/*
+		 * (@RequestParam String inputId)는 
+		 * String inputId=request.getParameter("inputId") 와 같다
+		 */
+		//서비스를 이용해서 해당 아이디가 존재하는지 여부를 알아낸다.
+		boolean isExist=service.isExistId(inputId);
+		//ModelAndView 객체에 해당 정보를 담고 view page 로 forward 이동해서 응답
+		mView.addObject("isExist",isExist);
+		mView.setViewName("users/checkid");
+		return mView;
+	}
+}
---
+++ Spring05_Final/src/main/java/com/gura/spring05/users/service/UsersService.java
@@ -0,0 +1,17 @@
+package com.gura.spring05.users.service;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.servlet.ModelAndView;
+
+import com.gura.spring05.users.dto.UsersDto;
+
+public interface UsersService {
+	//회원 가입 처리를 하는 메소드
+	public void addUser(UsersDto dto);
+	//아이디가 존재하는지 여부를 리턴하는 메소드
+	public boolean isExistId(String inputId);
+	//로그인폼에 관련된 처리를 하는 메소드
+	public void loginformLogic(HttpServletRequest request,
+			ModelAndView mView);
+}
